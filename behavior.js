1f5c37f5ef9ec7683cdf83a4a742743028aa0952
yizewang2006 <yizewangus@gmail.com>
2025-10-19 22:08:39 -0400

    Implemented QuickSelect Algorithm

--- behavior.js
+++ behavior.js
@@ -20,12 +20,18 @@ class Property {
 }
 
 export class Speed extends Property {
-    
+    get(agent) {
+
+    }
+
+    compare(self, target) {
+
+    }
 }
 
-export class Position extends Property{
+export class Position extends Property{ // This will be the Position class the agent is using, replaces previous one
     constructor(x, y) {
-        super();
+        super(); // call super's constructor
         this.x = x;
         this.y = y;
     }
@@ -37,12 +43,63 @@ export class Position extends Property{
     }
 
     get(agent) {
-        return agent.positon;
+        return agent.position;
     }
 
-    compare(self, target) {
-        // yet to implement
+    findNthClosest(self, targets, n) { // Will return the Agent that is nth closest to self from targets
+        // self = Agent
+        // targets = Agent Array
+        // n = int, indicating which one
+
+        // Using Quick Select Algorithm to find nth closest Agent
+
+        if (!targets || targets.length == 0) {
+            console.log("There are no agents available for detection");
+            return null;
+        }
+
+        if (n < 1 || n > targets.length) {
+            console.log("invalid n");
+            return null;
+        }
+
+        return this.quickSelect(targets, n-1, self);
     }
+    
+    quickSelect(targets, n, self) {
+        if (targets.length === 1) return targets[0]; // if there's only one, return that (that must be the closest)
+
+        const pivot = targets[Math.floor(Math.random() * targets.length)] // select a pivot
+        const pivotDistFromSelf = self.position.distanceTo(pivot.position);
+        
+        const left = [], equal = [], right = [];
+
+        for (let agent of targets) {
+            let d = self.position.distanceTo(agent.position);
+            if (d < pivotDistFromSelf) left.push(agent);
+            else if (d > pivotDistFromSelf) right.push(agent);
+            else equal.push(agent);
+        }
+
+        if (n < left.length) {
+            return this.quickSelect(left, n, self);
+        } else if (n < left.length + equal.length) {
+            return pivot;
+        } else {
+            return this.quickSelect(right, n - left.length - equal.length, self);
+        }
+    }
+
+    distanceTo(position) {
+        const diffX = this.x - position.x;
+        const diffY = this.y - position.y
+        // Corrected by ChatGPT for Warping
+        if (diffX > width / 2) diffX = width - diffX;
+        if (diffY > height / 2) diffY = height - diffY;
+
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
 }
 
 export class Angle extends Property {
